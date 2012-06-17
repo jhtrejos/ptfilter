07dde051e6b47f9fdcfae95db8bbb2a890a3a6cc
Sergey (dmi3s) Dmitriev <dmi3evsv@gmail.com>
2012-06-17 09:35:22 +0400

    test getopt()

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <pcre.h>
+#include <getopt.h>
+
 
 int main(int argc, char *argv[])
 {
+  if( argc != 2 ) {
+    fprintf(stderr, "Usage: %s pattern-specification\n", argv[0]);
+    return argc != 1;
+  }
+
+  char *pattern = argv[1];
+
   return 0;
 }
